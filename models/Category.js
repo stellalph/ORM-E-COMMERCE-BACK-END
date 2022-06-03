c2dd5d3aa4e9214cb1070b03c476ab69cad33e2c
Stella Ling <stellalph2@gmail.com>
2022-06-03 22:27:41 +0930

    add codes to category.js

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,19 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // define an id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull : false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // define define a category_name column
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+
   },
   {
     sequelize,
